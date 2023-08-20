c5c0e31485e4c7bd7622dd5a085223e5817108b5
pragralearning <pragralearning110@gmail.com>
2023-08-19 20:24:05 -0400

    initial commit

---
+++ src/main/java/co/itcloudinfo/usermanagement/rest/UserAPI.java
@@ -0,0 +1,15 @@
+package co.itcloudinfo.usermanagement.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserAPI {
+
+    @GetMapping("/get")
+    public String getUser(){
+        return "Sahil Hirpara";
+    }
+}
